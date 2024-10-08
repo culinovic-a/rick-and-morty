6735a44394216775793bc03c1f32e39bab8788ed
aleksandracul <aleksandraculinovic@gmail.com>
2024-10-08 21:49:49 +0200

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    /** @type {import('tailwindcss').Config} */
+    module.exports = {
+      content: [
+        "./src/**/*.{js,jsx,ts,tsx}",  // Paths to all your component files
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
