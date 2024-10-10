4676d71cdfd2e3f8505fb221e89af877b2eb5f02
aleksandracul <aleksandraculinovic@gmail.com>
2024-10-10 15:05:22 +0200

    fetch characters

--- src/Pages/CharactersPage/Characters.tsx
+++ src/Pages/CharactersPage/Characters.tsx
@@ -1,11 +1,123 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface ApiResponse {
+    info: Info,
+    results: Array<Character>
+}
+
+interface Info {
+    count: number;
+    next: string | null;
+    prev: string | null;
+    pages: number;
+}
+
+interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+        name: string;
+        url: string;
+    },
+    location: {
+        name: string;
+        url: string;
+    },
+    image: string;
+    episode: string[],
+    url: string;
+    created: string;
+}
 
 const Characters: React.FC = () => {
-  return (
-    <>
-     <div>characters page</div>
-    </>
-  );
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [info, setInfo] = useState<Info | null>(null);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [hasMore, setHasMore] = useState<boolean>(true);
+
+    const fetchCharacters = async (page: number) => {
+        if (loading) return;
+        setLoading(true);
+        try {
+            const apiUrl = process.env.REACT_APP_API_URL;
+
+            if (!apiUrl) {
+                throw new Error('API URL err');
+            }
+
+            const response = await axios.get<ApiResponse>(`${apiUrl}/character?page=${page}`);
+            setCharacters((prev) => [...prev, ...response.data.results]);
+            setInfo(response.data.info);
+            setHasMore(!!response.data.info.next);
+        } catch (err) {
+            setError(err instanceof Error ? err.message : 'An error occurred');
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleScroll = useCallback(() => {
+        if (loading || !hasMore) return;
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+        if (scrollTop + clientHeight >= scrollHeight - 5) {
+            setCurrentPage((prev) => prev + 1);
+        }
+    }, [loading, hasMore]);
+
+    useEffect(() => {
+        fetchCharacters(currentPage);
+    }, [currentPage]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
+
+    useEffect(() => {
+        setLoading(true);
+    }, [currentPage]);
+
+    if (loading && currentPage === 1) {
+        return <div>Loading characters...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <div>
+            <div className="max-w-2xl mx-auto text-center py-8">
+                <h1 className="text-3xl font-bold mb-4">Character List</h1>
+                <input
+                    type="text"
+                    className="border border-gray-300 rounded-lg py-2 px-4 mb-6 w-4/5"
+                    placeholder="Search characters..."
+                />
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {characters.map((character) => (
+                        <div key={character.id} className="bg-white border border-gray-300 rounded-lg shadow p-4">
+                            <h2>{character.name}</h2>
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {loading && <div>Loading more characters...</div>}
+        </div>
+    );
 };
 
 export default Characters;
